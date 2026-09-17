119c9d43db1edb55bcf50a4bdedc8021bf6d0de3
SiryyWolk <vladlysak01032006@gmail.com>
2026-09-17 08:27:03 +0000

    update

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # This script runs the reconsan.py script with the specified parameters.
 # Usage: python Recon.py <IP_ADDRESS> <START_PORT> <END_PORT> <TIMEOUT> <VERBOSE>
 # Example: python Recon.py 192.168.1.10 -s 20 -e 1000 -t 200 -v
-#          python Recon.py example -s 443 -e 443 -t 200 -v
+#          python Recon.py example.com -s 443 -e 443 -t 200 -v
